e97580503b4edcadf4364b8091125e55f2310491
tetiana22 <134863156+tetiana22@users.noreply.github.com>
2023-10-28 11:49:47 +0000

    Deploying to gh-pages from @ tetiana22/js09@f90fcd01584a33ccbd702b4e8dab053a2b676128 🚀

---
+++ 01-color-switcher.27ed40e1.js
@@ -0,0 +1,2 @@
+const t={startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]")};let e=null;t.startBtn.addEventListener("click",(()=>{e=setInterval((()=>{document.body.style.background=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),t.startBtn.disabled=!0})),t.stopBtn.addEventListener("click",(()=>{clearInterval(e),t.startBtn.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.27ed40e1.js.map
